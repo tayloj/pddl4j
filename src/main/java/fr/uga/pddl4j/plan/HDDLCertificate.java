c9aae97ca6fce072fd0797e15d9241bf518bd0dd
humbert <humbert.fiorino@imag.fr>
2020-06-06 19:12:43 +0200

    Plan verification for pandaPIparser

---
+++ src/main/java/fr/uga/pddl4j/plan/HDDLCertificate.java
@@ -0,0 +1,31 @@
+package fr.uga.pddl4j.plan;
+
+import fr.uga.pddl4j.problem.Action;
+
+import java.util.*;
+
+public class HDDLCertificate extends SequentialPlan {
+
+    private List<Integer> operatorIDs = new ArrayList<>();
+
+    public HDDLCertificate() {
+        super();
+    }
+
+    public HDDLCertificate(final Plan plan) {
+        super(plan);
+    }
+
+    public List<Integer> getOperatorIDs() {
+        return operatorIDs;
+    }
+
+    public final boolean add(final int time, final Action action, final Integer id) {
+        if (this.isTimeSpecifierOutOfBound(time)) {
+            return false;
+        }
+        this.actions().add(time, action);
+        this.operatorIDs.add(time, id);
+        return true;
+    }
+}
